80f2a5edd566726fb9306869697b46f5dc3b61ef
Aidan Jhou <aidanjhou@gmail.com>
2026-06-17 16:48:53 +0800

    feat: fix input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,13 +2,22 @@ import * as React from "react"
 
 import { cn } from "src/lib/utils"
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
+        {...(!props?.value ? { "data-placeholder": "" } : {})}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "file:border-0 file:bg-transparent file:text-sm file:font-medium",
+          "w-full h-10 flex items-center justify-between px-3 py-2 rounded-md text-sm [&>span]:line-clamp-1 focus:outline-none transition-colors",
+          "bg-background border border-input text-foreground data-[placeholder]:text-muted-foreground cursor-text",
+          "hover:bg-primary/5 hover:border-primary hover:text-foreground hover:data-[placeholder]:text-foreground",
+          "focus:bg-primary/5 focus:border-primary focus:text-foreground focus:data-[placeholder]:text-foreground",
+          "disabled:bg-muted disabled:border-muted disabled:text-foreground disabled:data-[placeholder]:text-muted-foreground disabled:cursor-not-allowed",
           className
         )}
         ref={ref}
